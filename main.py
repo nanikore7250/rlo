8d244f405b3134ffbf82cd0d3cad5fe5e09aa1ba
nanikore7250 <tropicana428.orange@gmail.com>
2026-06-10 11:36:31 +0900

    初回のHTMLおよびPythonスクリプトを追加し、社内セキュリティ研修用コンテンツを作成

---
+++ main.py
@@ -0,0 +1,16 @@
+import webbrowser
+from time import sleep
+
+# TRAINING_URL = "https://example.com/training"  # TODO: 実際のURLに差し替え
+
+print("installing...", end="", flush=True)
+for i in range(1, 50):
+    print("#", end="", flush=True)
+    sleep(0.05)
+print()
+
+print("successfully installed!!!")
+sleep(1)
+print("本ファイルは研修用の無影響ファイルです。\n研修用Webページに移動します。")
+sleep(1)
+# webbrowser.open(TRAINING_URL)
