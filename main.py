6af46ecd8bc396c1b2e674851e8e3bf5f6e26583
nanikore7250 <tropicana428.orange@gmail.com>
2026-06-12 20:57:35 +0900

    create rlo file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import webbrowser
 from time import sleep
 
-# TRAINING_URL = "https://example.com/training"  # TODO: 実際のURLに差し替え
+TRAINING_URL = "https://nanikore7250.github.io/rlo/"  # TODO: 実際のURLに差し替え
 
 print("installing...", end="", flush=True)
 for i in range(1, 50):
@@ -13,4 +13,4 @@ print("successfully installed!!!")
 sleep(1)
 print("本ファイルは研修用の無影響ファイルです。\n研修用Webページに移動します。")
 sleep(1)
-# webbrowser.open(TRAINING_URL)
+webbrowser.open(TRAINING_URL)
